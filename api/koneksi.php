7de85e460b101de63985d40158a52207bf5a5cb5
ardiansyahraihan389-wq <ardiansyahraihan389@gmail.com>
2026-04-28 15:05:06 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,13 +1,18 @@
 <?php
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
 $user = "2JyXBCpdbGQ96PV.root";
-$pass = "Ukkz7GGZOO03gON9"; 
-$db   = "test"; 
+$pass = "Ukkz7GGZOO3gON9";
+$db   = "test";
 $port = 4000;
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+// Membuat objek koneksi
+$conn = mysqli_init();
 
-if (!$conn) {
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi dengan flag SSL
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
