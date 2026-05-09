7ff8d2fd4685ee1ad6064f4edd9227f077f8309a
ardiansyahraihan389-wq <ardiansyahraihan389@gmail.com>
2026-05-09 09:14:27 +0700

    Add API endpoint for book and borrowing queries

---
+++ api/uts.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-Type: application/json');
+require_once 'koneksi.php'; 
+
+$soal = isset($_GET['soal']) ? $_GET['soal'] : '1';
+$tahun = isset($_GET['tahun']) ? $_GET['tahun'] : '2022';
+
+$sql = "";
+if ($soal == '1') {
+    $sql = "SELECT * FROM buku";
+} elseif ($soal == '2') {
+    $sql = "SELECT * FROM peminjaman";
+} elseif ($soal == '3') {
+    $sql = "SELECT p.nama_peminjam, b.judul_buku, b.penulis, p.tanggal_pinjam FROM peminjaman p JOIN buku b ON p.buku_id = b.id";
+} elseif ($soal == '4') {
+    $sql = "SELECT p.nama_peminjam, b.judul_buku, b.penulis, p.tanggal_pinjam FROM peminjaman p JOIN buku b ON p.buku_id = b.id WHERE b.tahun_terbit = '$tahun'";
+} elseif ($soal == '5') {
+    $sql = "SELECT b.judul_buku, COUNT(*) as total_dipinjam FROM peminjaman p JOIN buku b ON p.buku_id = b.id WHERE p.tanggal_pinjam BETWEEN '2026-05-01' AND '2026-05-07' GROUP BY b.judul_buku";
+} else {
+    echo json_encode(["message" => "Soal tidak ditemukan"]);
+    exit;
+}
+
+$result = mysqli_query($conn, $sql);
+$data = [];
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    echo json_encode($data, JSON_PRETTY_PRINT);
+} else {
+    echo json_encode(["error" => mysqli_error($conn)]);
+}
+mysqli_close($conn);
